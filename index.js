1ee7350e2eb34663b9bc2584dc61524fb20686b5
Martin Kobro <loellis92@gmail.com>
2024-02-12 14:13:30 +0100

    Exercise 4.2 - Refactored the code. Also added delete and update blog posts api endpoints.

--- index.js
+++ index.js
@@ -1,62 +1,7 @@
-require("dotenv").config()
-const express = require("express")
-const app = express()
-const cors = require("cors")
-const mongoose = require("mongoose")
+const app = require("./app") 
+const config = require("./utils/config")
+const logger = require("./utils/logger")
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-blogSchema.set("toJSON", {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
-})
-
-const Blog = mongoose.model("Blog", blogSchema)
-
-const mongoUrl = process.env.MONGODB_URI
-mongoose.connect(mongoUrl)
-
-app.use(cors())
-app.use(express.json())
-
-app.get("/api/blogs", (request, response, next) => {
-  Blog.find({})
-    .then(blogs => response.json(blogs))
-    .catch(error => next(error))
-})
-
-app.post("/api/blogs", (request, response, next) => {
-  const blog = new Blog(request.body)
-
-  blog.save()
-    .then(result => response.status(201).json(result))
-    .catch(error => next(error))
-})
-
-// Error handler
-const errorHandler = (error, request, response, next) => {
-  console.error(error.message)
-
-  if (error.name === "CastError") {
-    return response.status(400).send({ error: "Malformed ID" })
-  } else if (error.name === "ValidationError") {
-    return response.status(400).json({ error: error.message })
-  }
-
-  next(error)
-}
-
-app.use(errorHandler)
-
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
