cb0fbf2178b3e7ec60912af8530b65c6b5924b6a
Martin Kobro <loellis92@gmail.com>
2024-02-12 12:24:29 +0100

    Exercise 4.1 - Added errorhandler and dotenv to properly process MongoDB address. Added run dev script to run nodemon locally. Tested with .rest-file and via postman, both working.

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const express = require('express')
+require("dotenv").config()
+const express = require("express")
 const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
+const cors = require("cors")
+const mongoose = require("mongoose")
 
 const blogSchema = new mongoose.Schema({
   title: String,
@@ -10,31 +11,50 @@ const blogSchema = new mongoose.Schema({
   likes: Number
 })
 
-const Blog = mongoose.model('Blog', blogSchema)
+blogSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
 
-const mongoUrl = 'mongodb://localhost/bloglist'
+const Blog = mongoose.model("Blog", blogSchema)
+
+const mongoUrl = process.env.MONGODB_URI
 mongoose.connect(mongoUrl)
 
 app.use(cors())
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
+app.get("/api/blogs", (request, response, next) => {
+  Blog.find({})
+    .then(blogs => response.json(blogs))
+    .catch(error => next(error))
 })
 
-app.post('/api/blogs', (request, response) => {
+app.post("/api/blogs", (request, response, next) => {
   const blog = new Blog(request.body)
 
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
+  blog.save()
+    .then(result => response.status(201).json(result))
+    .catch(error => next(error))
 })
+
+// Error handler
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "Malformed ID" })
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = 3003
 app.listen(PORT, () => {
